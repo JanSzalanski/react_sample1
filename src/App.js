7ebaaae7166e78beba7b04ddda6c80c85145e4b6
JanSzalanski <jan@szalanski.eu>
2021-12-05 23:07:45 +0100

    added expenses component with props & styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
+// import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -6,7 +7,7 @@ function App() {
       id: "e1",
       title: "Papier Toaletowy",
       amount: 150,
-      date: new Date(2020, 4, 14),
+      date: new Date(2020, 3, 14),
     },
     { id: "e2", title: "Dokupienie BTC", amount: 3830, date: new Date(2021, 7, 20) },
     {
@@ -19,17 +20,15 @@ function App() {
       id: "e4",
       title: "Koszta balangi",
       amount: 1800,
-      date: new Date(2021, 5, 10),
+      date: new Date(2021, 4, 10),
     },
   ];
 
   return (
     <div>
       <h2>Expense App</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+
+      <Expenses items={expenses} />
     </div>
   );
 }
