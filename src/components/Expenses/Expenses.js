1e34f646fbcace05405284fbb3d77c8b92f948ad
JanSzalanski <jan@szalanski.eu>
2021-12-09 14:03:27 +0100

    added chart component - end of project

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFlter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2021");
@@ -22,6 +23,7 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onAddYear={addYearHandler} />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
